6a6c9d70fe34458babddd60753d190569a33f320
timirtimir <tamerekim@yahoo.com>
2026-08-30 19:44:47 +0300

    Basic scene setup and added enemy dot placement for future hit detection

---
+++ Assets/Scripts/Game/Enemy.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+// Inheritence
+public class Enemy : MonoBehaviour
+{
+    protected float BlastCoverageThershold;
+    [SerializeField] private GameObject dotPrefab;
+    private BoxCollider boxC;
+    private Bounds bounds;
+    private float xCount = 5f;
+    private float yCount = 5f;
+    private float zCount = 4f;
+    //private int padding = 10;
+    private float xTotal;
+    private float yTotal;
+    private float zTotal;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        boxC = GetComponent<BoxCollider>();
+        bounds = boxC.bounds;
+        xTotal = bounds.max.x - bounds.min.x;
+        yTotal = bounds.max.y - bounds.min.y;
+        zTotal = bounds.max.z - bounds.min.z;
+        for (int x=0; x<xCount; x++)
+        {
+            for(int y=0; y<yCount; y++)
+            {
+                for(int z=0; z<zCount; z++)
+                {
+                    float xPos = (x / (xCount - 1f)) * xTotal + bounds.min.x;
+                    float yPos = (y / (yCount - 1f)) * yTotal + bounds.min.y;
+                    float zPos = (z / (zCount - 1f)) * zTotal + bounds.min.z;
+
+                    Vector3 dotPos = new Vector3(xPos, yPos, zPos);
+
+                    Instantiate(dotPrefab, dotPos, dotPrefab.transform.rotation, gameObject.transform);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
